4234cb3a8c5420e359124fc0e8d71be0f7572a20
Gabriel Rosmart <gabrielrostron@gmail.com>
2023-01-13 19:23:45 +0100

    Admin layout and dashboard

---
+++ resources/js/Layouts/AdminLayout.vue
@@ -0,0 +1,8 @@
+<script setup>
+    import AdminNavbar from '@/Shared/Navigation/AdminNavbar.vue';
+</script>
+
+<template>
+    <AdminNavbar/>
+    <slot/>
+</template>
---
+++ resources/js/Pages/Admin/Dashboard.vue
@@ -0,0 +1,8 @@
+<script setup>
+    import AdminLayout from '@/Layouts/AdminLayout.vue';
+</script>
+
+<template>
+    <AdminLayout>
+    </AdminLayout>
+</template>
