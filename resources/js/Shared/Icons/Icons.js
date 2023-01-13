9917898d8e561f14496a5bdb93866330028ce9a2
Gabriel Rosmart <gabrielrostron@gmail.com>
2023-01-13 18:28:21 +0100

    Added Icons

---
+++ resources/js/Shared/Icons/Icons.js
@@ -0,0 +1,9 @@
+import ManageUsersIcon from './ManageUsersIcon.vue'
+import SettingsIcon from './SettingsIcon.vue'
+import HomeIcon from './HomeIcon.vue'
+
+export {
+    ManageUsersIcon,
+    SettingsIcon,
+    HomeIcon
+}
