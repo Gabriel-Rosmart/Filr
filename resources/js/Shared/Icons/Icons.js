09902470b7333d814f70d903858e096ecaf5c54c
Gabriel Rosmart <gabrielrostron@gmail.com>
2023-01-13 19:03:22 +0100

    Added navbar

--- resources/js/Shared/Icons/Icons.js
+++ resources/js/Shared/Icons/Icons.js
@@ -1,9 +1,11 @@
 import ManageUsersIcon from './ManageUsersIcon.vue'
 import SettingsIcon from './SettingsIcon.vue'
 import HomeIcon from './HomeIcon.vue'
+import BellIcon from './BellIcon.vue'
 
 export {
     ManageUsersIcon,
     SettingsIcon,
-    HomeIcon
+    HomeIcon,
+    BellIcon
 }
---
+++ resources/js/Shared/Navigation/AdminNavbar.vue
@@ -0,0 +1,36 @@
+<script setup>
+    import { BellIcon } from '../Icons/Icons';
+</script>
+
+<template>
+    <div class="navbar bg-base-100">
+        <div class="navbar-start">
+            <div class="dropdown">
+                <label tabindex="0" class="btn btn-ghost btn-circle">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24"
+                        stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M4 6h16M4 12h16M4 18h7" />
+                    </svg>
+                </label>
+                <ul tabindex="0"
+                    class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                    <li><a>Homepage</a></li>
+                    <li><a>Manage Users</a></li>
+                    <li><a>Settings</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="navbar-center">
+            <a class="btn btn-ghost normal-case text-xl">daisyUI</a>
+        </div>
+        <div class="navbar-end">
+            <button class="btn btn-ghost btn-circle">
+                <div class="indicator">
+                    <BellIcon/>
+                    <span class="badge badge-xs badge-primary indicator-item"></span>
+                </div>
+            </button>
+        </div>
+    </div>
+</template>
