c90d5696b7b1c199fea8e2cbf867eb04b903567b
Gabriel Rosmart <gabrielrostron@gmail.com>
2023-01-14 20:11:13 +0100

    Added breadcrumbs

--- resources/js/Pages/Admin/Dashboard.vue
+++ resources/js/Pages/Admin/Dashboard.vue
@@ -2,10 +2,12 @@
     import AdminLayout from '@/Layouts/AdminLayout.vue';
     import QueryFilters from '@/Shared/Filters/QueryFilters.vue';
     import Pagination from '@/Shared/Filters/Pagination.vue';
+    import Breadcrumbs from '@/Shared/Navigation/Breadcrumbs.vue'
 </script>
 
 <template>
     <AdminLayout>
+        <Breadcrumbs class="ml-5 mt-6"/>
         <div class="flex items-center mt-8">
             <QueryFilters/>
             <Pagination class="ml-24"/>
