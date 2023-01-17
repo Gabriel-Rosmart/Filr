2cdd3b0226814ded0a62bdf56bedaaab73e8e680
Gabriel Rosmart <gabrielrostron@gmail.com>
2023-01-17 17:49:05 +0100

    Admin route and controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
@@ -17,6 +18,8 @@ use Inertia\Inertia;
 */
 
 Route::redirect('/', '/login');
+
+Route::get('/admin', [AdminController::class, 'index']);
 
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
