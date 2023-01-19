e42e5e114a9c74839c81784c5d8fb9177a38f128
Gabriel-Rosmart <66219601+Gabriel-Rosmart@users.noreply.github.com>
2023-01-19 13:34:59 +0100

    Added vue-i18n optimization

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,12 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
+// ! Uncoment when app ready to deploy, meanwhile work whit this commented, as make page reload faster
+/* 
+import { resolve, dirname } from 'node:path'
+import { fileURLToPath } from 'url'
+import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite'
+*/
 export default defineConfig({
     plugins: [
         laravel({
@@ -16,5 +22,9 @@ export default defineConfig({
                 },
             },
         }),
+        /*
+        VueI18nPlugin({
+            include: resolve(dirname(fileURLToPath(import.meta.url)), './resources/js/Locales/**'),
+        }),*/
     ],
 });
