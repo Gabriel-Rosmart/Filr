bbd09dfb4f1b84fdcc6c923604539e8007b670ae
Raul Arias Perez <aprariasperez@gmail.com>
2023-01-23 15:18:42 +0100

    forgot a semicolon kappa

--- resources/js/Pages/Admin/UserDetails.vue
+++ resources/js/Pages/Admin/UserDetails.vue
@@ -1,11 +1,12 @@
 <script setup>
-    import AdminLayout from '@/Layouts/AdminLayout.vue'
-    import Breadcrumbs from '@/Shared/Navigation/Breadcrumbs.vue';
+import AdminLayout from '@/Layouts/AdminLayout.vue';
+import Breadcrumbs from '@/Shared/Navigation/Breadcrumbs.vue';
 </script>
 
 <template>
     <AdminLayout>
-        <Breadcrumbs class="ml-5 mt-6" :pages="['Admin', 'Manage Users', 'User Details']"/>
+        <Breadcrumbs class="ml-5 mt-6" :pages="['Admin', 'Manage Users', 'User Details']" />
 
+        
     </AdminLayout>
 </template>
