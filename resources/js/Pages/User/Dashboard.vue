431ec18a39608c3d4154486cb307e05e32fd7650
Álvaro Sierra <asp.sierra@gmail.com>
2023-01-24 12:34:09 +0100

    early User Dashboard

--- resources/js/Pages/User/Dashboard.vue
+++ resources/js/Pages/User/Dashboard.vue
@@ -1,8 +1,42 @@
 <script setup>
 import UserLayout from '@/Layouts/UserLayout.vue';
+import Breadcrumbs from '@/Shared/Navigation/Breadcrumbs.vue';
 </script>
 
 <template>
     <UserLayout>
+        <Breadcrumbs class="ml-5 mt-6" :pages="['User', 'Dashboard']" />
+        <div class="flex justify-center content-center">
+            <ul class="menu menu-horizontal bg-base-100 rounded-box">
+                <li>
+                    <button class="active">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="active h-5 w-96" fill="none" viewBox="0 0 24 24"
+                            stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
+                        </svg>
+                    </button>
+                </li>
+                <li>
+                    <button>
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-96" fill="none" viewBox="0 0 24 24"
+                            stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                        </svg>
+                    </button>
+                </li>
+                <li>
+                    <button>
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-96" fill="none" viewBox="0 0 24 24"
+                            stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z" />
+                        </svg>
+                    </button>
+                </li>
+            </ul>
+        </div>
+
     </UserLayout>
 </template>
