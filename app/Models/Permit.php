c5ffa362d4cf144cf3f92fc14b27f7fd04de6212
Gabriel Rosmart <gabrielrostron@gmail.com>
2023-01-24 19:32:17 +0100

    User-Permit relationship

--- app/Models/Permit.php
+++ app/Models/Permit.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Permit extends Model
 {
     use HasFactory;
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
