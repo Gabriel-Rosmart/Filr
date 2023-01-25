7997362e373c3c0fcf8d394e52e23c6238559b0e
Gabriel Rosmart <gabrielrostron@gmail.com>
2023-01-25 16:53:31 +0100

    User-Role relationship

--- app/Models/User.php
+++ app/Models/User.php
@@ -46,4 +46,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(Permit::class);
     }
+
+    public function role()
+    {
+        return $this->belongsTo(Role::class);
+    }
 }
