81b3393b681a12cbebd4001508eb1a17975b8bef
Raul Arias Perez <aprariasperez@gmail.com>
2023-01-25 10:26:44 +0100

    Minor changes on UserDetails

--- resources/js/Pages/Admin/UserDetails.vue
+++ resources/js/Pages/Admin/UserDetails.vue
@@ -1,12 +1,21 @@
 <script setup>
 import AdminLayout from '@/Layouts/AdminLayout.vue';
 import Breadcrumbs from '@/Shared/Navigation/Breadcrumbs.vue';
+import PermitsTable from "@/Shared/Info/PermitsTable.vue";
 </script>
 
 <template>
     <AdminLayout>
         <Breadcrumbs class="ml-5 mt-6" :pages="['Admin', 'Manage Users', 'User Details']" />
-
+        <div class="flex justify-center content-center">
+            <div class="flex flex-col mx-20 w-full">
+                <div></div>
+                <div class="divider divider-vertical"></div>
+                <div>
+                    <PermitsTable />
+                </div>
+            </div>
+        </div>
         
     </AdminLayout>
 </template>
