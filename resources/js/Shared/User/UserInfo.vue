5ce4228dc96d6863ba61bf7e6712c98221878dab
Álvaro Sierra <asp.sierra@gmail.com>
2023-01-25 11:00:12 +0100

    display an user information

---
+++ resources/js/Shared/User/UserInfo.vue
@@ -0,0 +1,38 @@
+<script setup>
+
+const userData = {
+    name: "Pepe",
+    surname: "Pepez Pepez",
+    email: "pepe@pepez.ez",
+    image: "https://imgv3.fotor.com/images/blog-cover-image/10-profile-picture-ideas-to-make-you-stand-out.jpg"
+}
+
+
+
+
+</script>
+
+
+<template>
+    <div class="flex justify-start">
+        <div class="avatar online">
+            <div class="w-24 rounded-full">
+                <img :src="userData.image" />
+            </div>
+        </div>
+        <div>
+            <div class="flex gap-4 items-end">
+                <span class="text-5xl">{{ userData.name }}</span>
+                <span class="text-2xl">{{ userData.surname }}</span>
+            </div>
+            <div>
+                <span>
+                    &lt {{ userData.email }} &gt
+                </span>
+            </div>
+
+        </div>
+    </div>
+
+
+</template>
