3fed4963fa5976ec1286aff1729950aab9da4621
Raul Arias Perez <aprariasperez@gmail.com>
2023-01-26 10:18:31 +0100

    added new bread to managePermits

--- resources/js/Pages/Admin/ManagePermits.vue
+++ resources/js/Pages/Admin/ManagePermits.vue
@@ -1,7 +1,7 @@
 <script setup>
     import AdminLayout from '@/Layouts/AdminLayout.vue'
     import PermitsTable from '@/Shared/Info/PermitsTable.vue';
-    import Breadcrumbs from '@/Shared/Navigation/BreadcrumbsLegacy.vue';
+    import Breadcrumbs from '@/Shared/Navigation/Breadcrumbs.vue';
     import Pagination from '@/Shared/Filters/Pagination.vue';
     import NameFilter from '@/Shared/Filters/NameFilter.vue';
 
@@ -12,7 +12,7 @@
 
 <template>
     <AdminLayout>
-        <Breadcrumbs class="ml-5 mt-6" :pages="['Admin', 'Permits']"/>
+        <Breadcrumbs class="ml-5 mt-6" :pages="[['Admin', '/admin'], ['Permits', '/admin/permits']]"/>
         <div class="flex items-center mt-8">
             <NameFilter/>
             <Pagination class="ml-4"/>
