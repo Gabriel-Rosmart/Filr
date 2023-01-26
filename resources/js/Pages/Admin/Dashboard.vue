fd703c55e7324cb556d920cc0c78d15fe7a08b05
Raul Arias Perez <aprariasperez@gmail.com>
2023-01-26 10:12:41 +0100

    Added new bread to dashboard

--- resources/js/Pages/Admin/Dashboard.vue
+++ resources/js/Pages/Admin/Dashboard.vue
@@ -2,13 +2,13 @@
     import AdminLayout from '@/Layouts/AdminLayout.vue';
     import QueryFilters from '@/Shared/Filters/QueryFilters.vue';
     import Pagination from '@/Shared/Filters/Pagination.vue';
-    import Breadcrumbs from '@/Shared/Navigation/BreadcrumbsLegacy.vue'
+    import Breadcrumbs from '@/Shared/Navigation/Breadcrumbs.vue'
     import FilesTable from '@/Shared/Info/FilesTable.vue';
 </script>
 
 <template>
     <AdminLayout>
-        <Breadcrumbs class="ml-5 mt-6" :pages="['Admin', 'Dashboard']"/>
+        <Breadcrumbs class="ml-5 mt-6" :pages="[['Admin', '/admin'], ['Dashboard', '/admin']]"/>
         <div class="flex items-center mt-8">
             <QueryFilters url="/admin"/>
             <Pagination class="ml-24"/>
