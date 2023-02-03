77416ecb27396c882ae11ca013651a14cb64dd66
Raúl Arias Pérez <aprariasperez@gmail.com>
2023-02-03 19:39:40 +0100

    changing timetable to horizontal display

--- resources/js/Shared/Info/TimeTable.vue
+++ resources/js/Shared/Info/TimeTable.vue
@@ -43,11 +43,37 @@
         timetable: Object
     })
 
+    const week = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
+
     console.log(props);
 </script>
 
 <template>
     <div>
+        <table class="table w-full">
+            <thead>
+                <tr class="hover">
+                    <th>Day</th>
+                    <th v-for="day of week">{{ day }}</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th>In</th>
+                </tr>
+                <tr>
+                    <th>Out</th>
+                </tr>
+                <tr>
+                    <th>In</th>
+                </tr>
+                <tr>
+                    <th>Out</th>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    <!--<div>
         <table class="table w-full">
             <thead>
                 <tr>
@@ -65,26 +91,6 @@
                     <td>{{ day.ends_at }}</td>
                     <td>{{ day.in2 }}</td>
                     <td>{{ day.out2 }}</td>
-                </tr>
-            </tbody>
-        </table>
-    </div>
-    <!--<div>
-        <table class="table w-full">
-            <thead>
-                <tr>
-                    <th>Day</th>
-                    <th>start</th>
-                    <th>end</th>
-                    <th>range id</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr v-for="day of timetable" class="hover">
-                    <th>{{ day.day }}</th>
-                    <td>{{ day.starts_at }}</td>
-                    <td>{{ day.ends_at }}</td>
-                    <td>{{ day.date_range_id }}</td>
                 </tr>
             </tbody>
         </table>
