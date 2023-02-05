a57a5bf4d69879efd9bbf5072a9dc67672346106
Gabriel Rosmart <gabrielrostron@gmail.com>
2023-02-05 18:12:09 +0100

    Created tabs component

---
+++ resources/js/Shared/Navigation/Tabs.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+    /** Component imports */
+    import { Link } from '@inertiajs/inertia-vue3';
+
+    defineProps({
+        active: Number,
+    })
+</script>
+
+<template>
+    <div class="tabs">
+        <Link href="/admin" class="tab tab-bordered" :class="{'tab-active': active == 0}">Hoy</Link>
+        <Link href="/test" class="tab tab-bordered" :class="{'tab-active': active == 1}">Todos</Link>
+    </div>
+</template>
