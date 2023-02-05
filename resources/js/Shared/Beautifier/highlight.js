fe2e3d72b87332e864850cd926b286bbd4105a07
Gabriel Rosmart <gabrielrostron@gmail.com>
2023-02-05 17:49:00 +0100

    Deleted unecessary beautifier files

--- resources/js/Shared/Beautifier/highlight.js
+++
@@ -1,6 +0,0 @@
-export default function highlight (text, reg) {
-    if(reg == null) return text
-    return text.replace(new RegExp(reg, "gi"), match => {
-        return '<span class="text-red-600">' + match + '</span>'
-    })
-}
