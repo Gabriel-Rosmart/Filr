77800e558b9f1ff5bc4399de4f1b7ac3240865a2
Raul Arias Perez <aprariasperez@gmail.com>
2023-02-07 11:26:04 +0100

    UserFurther component created

--- resources/js/Pages/Admin/UserDetails.vue
+++ resources/js/Pages/Admin/UserDetails.vue
@@ -6,7 +6,7 @@ import UserInfo from '@/Shared/User/UserInfo.vue';
 
 import { Head } from '@inertiajs/inertia-vue3';
 
-import TestTable from '@/Shared/Info/TestTable.vue'
+import Further from '@/Shared/User/UserFurther.vue'
 import { ref } from 'vue'
 
 const props = defineProps({
@@ -14,7 +14,7 @@ const props = defineProps({
     timetable: Object
 })
 
-const tabs = [TimeTable, TestTable]
+const tabs = [TimeTable, Further]
 let currentComponentIndex = ref(0)
 
 </script>
---
+++ resources/js/Shared/User/UserFurther.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="stats stats-vertical shadow bg-slate-200 dark:bg-slate-800">
+        <div class="stat">
+           <div class="stat-title">DNI</div>
+           <div class="stat-value">39999999A</div>
+        </div>
+        <div class="stat">
+            <div class="stat-title">Phone</div>
+            <div class="stat-value">+34 000 000 000</div>
+        </div>
+        <div class="stat">
+            <div class="stat-title">Puesto</div>
+            <div class="stat-value">Administrativo</div>
+        </div>
+        <div class="stat">
+            <div class="stat-title">Email</div>
+            <div class="stat-value">correo@example.com</div>
+        </div>
+    </div>
+</template>
