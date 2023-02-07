98154b66b53899bda88b65a243ec8a73ab8440af
Gabriel Rosmart <gabrielrostron@gmail.com>
2023-02-07 19:01:58 +0100

    Updated incidence message formatter

--- resources/js/Utilities/incidence.ts
+++ resources/js/Utilities/incidence.ts
@@ -1,7 +1,7 @@
 import trans from './trans'
 
 export default function getIncidenceMessage(subject: string, minutes: number):string {
-    if(subject === 'absent') return trans(subject, 'es')
+    if(subject === 'absent') return `<span style="color: red">${trans(subject, 'es')}</span>`
     let highlight = `<span style="color: red">${minutes}</span>`
     if(subject === 'early') return `Sale ${highlight} minutos ${trans(subject, 'es')}`
     if(subject === 'late') return `Llega ${highlight} minutos ${trans(subject, 'es')}`
