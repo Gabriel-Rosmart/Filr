2f1d460bd10627fdb80a1faaaa2fdf493b95b4ec
Raul Arias Perez <aprariasperez@gmail.com>
2023-02-07 09:24:28 +0100

    userWeek not iterable ?

--- resources/js/Shared/Info/TimeTable.vue
+++ resources/js/Shared/Info/TimeTable.vue
@@ -1,39 +1,20 @@
 <script setup>
+    import { ref } from 'vue'
 
     const props = defineProps({
         timetable: Object
     })
 
-    const week = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
+    var userWeek = {'monday': [], 'tuesday': [], 'wednesday': [], 'thursday': [], 'friday': []};
 
-    console.log(props);
+    for (const day of userWeek)
+        for (const shift of props.timetable)
+            if (shift.day == day)
+                userWeek[day].push(shift);
+    
 </script>
 
 <template>
-    <!-- <div>
-        <table class="table w-full">
-            <thead>
-                <tr class="hover">
-                    <th>Day</th>
-                    <th v-for="day of week">{{ day }}</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <th>In</th>
-                </tr>
-                <tr>
-                    <th>Out</th>
-                </tr>
-                <tr>
-                    <th>In</th>
-                </tr>
-                <tr>
-                    <th>Out</th>
-                </tr>
-            </tbody>
-        </table>
-    </div> -->
     <div>
         <table class="table w-full">
             <thead>
@@ -46,12 +27,12 @@
                 </tr>
             </thead>
             <tbody>
-                <tr v-for="day of timetable" class="hover">
-                    <th>{{ day.day }}</th>
-                    <td>{{ day.starts_at }}</td>
-                    <td>{{ day.ends_at }}</td>
-                    <td>{{ day.in2 }}</td>
-                    <td>{{ day.out2 }}</td>
+                <tr v-for="day of userWeek" class="hover">
+                    <th>{{ day[0].day }}</th>
+                    <td>{{ day[0].starts_at }}</td>
+                    <td>{{ day[0].ends_at }}</td>
+                    <td>{{ day[1].starts_at }}</td>
+                    <td>{{ day[1].ends_at }}</td>
                 </tr>
             </tbody>
         </table>
