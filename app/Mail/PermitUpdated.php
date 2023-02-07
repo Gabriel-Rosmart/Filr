38d585078b2b06a7ee955037f7726d904b4686b4
Gabriel Rosmart <gabrielrostron@gmail.com>
2023-02-07 19:10:17 +0100

    Permit mail class

---
+++ app/Mail/PermitUpdated.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class PermitUpdated extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    protected $uuid;
+    protected $status;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct(string $uuid, string $status)
+    {
+        $this->uuid = $uuid;
+        $this->status = $status;
+    }
+
+    /**
+     * Get the message envelope.
+     *
+     * @return \Illuminate\Mail\Mailables\Envelope
+     */
+    public function envelope()
+    {
+        return new Envelope(
+            subject: 'Permit Updated',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     *
+     * @return \Illuminate\Mail\Mailables\Content
+     */
+    public function content()
+    {
+        return new Content(
+            view: 'email.permit',
+            with: [
+                'uuid' => $this->uuid,
+                'status' => $this->status
+            ]
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array
+     */
+    public function attachments()
+    {
+        return [];
+    }
+}
