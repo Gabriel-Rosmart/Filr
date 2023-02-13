cf3fdd8eb9cc5fe577480048571c5a2cd6b2b028
Raul Arias Perez <aprariasperez@gmail.com>
2023-02-13 10:24:01 +0100

    accept-deny permits added

--- resources/js/Shared/Info/PermitsAdminTable.vue
+++ resources/js/Shared/Info/PermitsAdminTable.vue
@@ -1,17 +1,49 @@
 <script setup>
+
+    /** Icon imports */
+    import { CheckIcon, XIcon } from "../Icons/Icons";
+
+    /** Component imports */
+    import { Link } from "@inertiajs/inertia-vue3";
+
+    /** Function imports */
+    import { Inertia } from "@inertiajs/inertia";
+    import trans from "@/Utilities/trans";
     import format from '@/Utilities/datefm'
-    const props = defineProps({
+
+    import { useI18n } from 'vue-i18n'
+
+    const { t, d } = useI18n()
+
+    defineProps({
         permits: Array
     })
+
+    const accept = (pid) => {
+        Inertia.post('/permits', {
+            uuid: pid,
+            status: 'accepted'
+        })
+    }
+
+    const deny = (pid) => {
+        Inertia.post('/permits', {
+            uuid: pid,
+            status: 'denied'
+        })
+    }
+
 </script>
+
 <template>
-    <div class="overflow-x-autp">
+    <div class="overflow-x-auto w-full">
         <table class="table w-full">
             <thead>
                 <tr>
                     <th>PID</th>
-                    <th>Requested At</th>
-                    <th>Status</th>
+                    <th>{{ t('table.reqby') }}</th>
+                    <th>{{ t('table.reqat') }}</th>
+                    <th></th>
                 </tr>
             </thead>
             <tbody>
@@ -20,11 +52,24 @@
                         {{ permit.uuid }}
                     </td>
                     <td>
-                        {{ format(permit.requested_at) }}
+                        {{ d(permit.requested_at, 'short') }}
                     </td>
-                    <td>
-                        {{ permit.status }}
-                    </td>
+                    <th v-if="permit.status === 'pending'">
+                        <div>
+                            <button class="btn btn-outline btn-success" @click="accept(permit.uuid)">{{ t('admin.buttons.accept') }}</button>
+                            <button class="btn btn-outline btn-error ml-8" @click="deny(permit.uuid)">{{ t('admin.buttons.deny') }}</button>
+                        </div>
+                    </th>
+                    <th v-else>
+                        <div v-if="permit.status === 'accepted'" class="flex">
+                            <CheckIcon class="stroke-green-500"/>
+                            <span class="ml-6">{{ t(`admin.permitstate.${permit.status}`) }}</span>
+                        </div>
+                        <div v-else class="flex">
+                            <XIcon class="stroke-red-500"/>
+                            <span class="ml-6">{{ t(`admin.permitstate.${permit.status}`) }}</span>
+                        </div>
+                    </th>
                 </tr>
             </tbody>
         </table>
