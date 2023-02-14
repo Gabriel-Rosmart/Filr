0e90e823d219b4a6a485b868b2df2a91d6b433b2
Raul Arias Perez <aprariasperez@gmail.com>
2023-02-14 14:23:44 +0100

    further language changes

--- resources/js/Shared/User/UserFurther.vue
+++ resources/js/Shared/User/UserFurther.vue
@@ -20,7 +20,7 @@
         </div>
         <div class="stat">
             <div class="stat-title">{{ t('admin.details.user.position') }}</div>
-            <div class="stat-value">{{ user.role.role_name }}</div>
+            <div class="stat-value">{{ t('admin.details.user.posType.' + user.role.role_name) }}</div>
         </div>
         <div class="stat">
             <div class="stat-title">{{ t('admin.details.user.email') }}</div>
