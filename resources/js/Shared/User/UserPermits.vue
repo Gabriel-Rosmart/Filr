e5b6081cee542c311521b15f153453a021523108
Álvaro Sierra <asp.sierra@gmail.com>
2023-02-15 09:39:32 +0100

    message when no permits found

--- resources/js/Shared/User/UserPermits.vue
+++ resources/js/Shared/User/UserPermits.vue
@@ -10,7 +10,7 @@ defineProps({
 </script>
 
 <template>
-    <div class="overflow-x-auto w-full">
+    <div v-if="permits.length > 0" class="overflow-x-auto w-full">
         <table class="table w-full">
             <thead>
                 <tr>
@@ -41,5 +41,17 @@ defineProps({
             </tbody>
         </table>
 
+    </div>
+    <div v-else>
+        <div class="alert alert-info shadow-lg">
+            <div>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                    class="stroke-current flex-shrink-0 w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                        d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+                </svg>
+                <span>No permits found</span>
+            </div>
+        </div>
     </div>
 </template>
