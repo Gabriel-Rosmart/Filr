58edeb4bf7f7c4ac5e6c7e3ecf660229d1c6a240
Raul Arias Perez <aprariasperez@gmail.com>
2023-02-16 10:42:04 +0100

    language changes & check if exist files

--- resources/js/Shared/Info/PermitsAdminTable.vue
+++ resources/js/Shared/Info/PermitsAdminTable.vue
@@ -86,7 +86,7 @@ const deny = (pid) => {
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                         d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
                 </svg>
-                <span>No permits found</span>
+                <span>{{ t('admin.details.noPermit') }}</span>
             </div>
         </div>
     </div>
