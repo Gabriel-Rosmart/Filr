55ec162ae6117f46bf1679805bc1936d66d16c49
Javier Mosquera <a21javiermr@iesteis.es>
2023-02-16 12:55:46 +0100

    Edited edituser layout

--- resources/js/Shared/Forms/FormEditUser.vue
+++ resources/js/Shared/Forms/FormEditUser.vue
@@ -28,7 +28,7 @@ var form = useForm({
 
 var date = useForm({
     starts_at: props.timetable.starts_at,
-    ends_at:props.timetable.ends_at
+    ends_at: props.timetable.ends_at
 
 });
 
@@ -42,60 +42,58 @@ const submit = () => {
 
 
 <template>
-    <div class="flex items-center mt-8">
-        <form @submit.prevent="">
+    <div class="flex items-center w-full mt-8">
+        <form @submit.prevent="" class="w-full pr-10">
             <div class="grid grid-cols-2 w-auto">
-                <div>
-                    <div class=" grid grid-cols-2 w-max py-10 px-10 ">
-                        <div class="grid grid-cols-2 w-max gap-6">
-                            <div class="grid row-span-3 justify-center items-center">
-                                <div class="avatar">
-                                    <div class="mask mask-squircle w-60 h-60">
-                                        <img :src="storage + user.profile_pic" alt="Profile pic" />
-                                    </div>
+                <div class=" grid justify-items-center py-10 pl-10 ">
+                    <div class="grid grid-cols-2 w-max gap-6">
+                        <div class="grid row-span-3 justify-center items-center">
+                            <div class="avatar">
+                                <div class="mask mask-squircle w-60 h-60">
+                                    <img :src="storage + user.profile_pic" alt="Profile pic" />
                                 </div>
                             </div>
-                            <InputForm title="Email" type="email" v-model="form.email" :value='user.email' />
-                            <InputForm title="DNI" type="text" v-model="form.dni" :value='user.dni' />
-                            <InputForm title="Phone" type="telephone" v-model="form.telephone" :value='user.phone' />
-                            <SelectForm title="Role" :data='3' :rol="user.role_id" />
-                            <InputForm title="Name" type="text" v-model="form.name" :value='user.name' />
                         </div>
-                    </div>
-                    <div class="flex justify-end mt-8">
-                        <button class="btn btn-outline btn-error mr-4">Cancel</button>
-                        <button class="btn btn-outline btn-success" @click="submit">Save</button>
+                        <InputForm title="Email" type="email" v-model="form.email" :value='user.email' />
+                        <InputForm title="DNI" type="text" v-model="form.dni" :value='user.dni' />
+                        <InputForm title="Phone" type="telephone" v-model="form.telephone" :value='user.phone' />
+                        <SelectForm title="Role" :data='3' :rol="user.role_id" />
+                        <InputForm title="Name" type="text" v-model="form.name" :value='user.name' />
                     </div>
                 </div>
-                <div class="pr-40">
-                    <table class="table w-full">
-                                <thead>
-                                    <tr class="">
-                                        <th></th>
-                                        <th>In (Morning)</th>
-                                        <th>Out (Morning)</th>
-                                        <th>In (Afternoon)</th>
-                                        <th>Out (Afternoon)</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr v-for="day in weekend">
-                                        <th>{{ day.day }}</th>
-                                        <td>
-                                            <InputForm type="time" :value='day.starts_at' />
-                                        </td>
-                                        <td>
-                                            <InputForm type="time" :value='day.ends_at' />
-                                        </td>
-                                        <td>
-                                            <InputForm type="time" :value='day.starts_at' />
-                                        </td>
-                                        <td>
-                                            <InputForm type="time" :value='day.ends_at' />
-                                        </td>
-                                    </tr>
-                                </tbody>
-                            </table>
+                <div class="flex justify-center">
+                    <table class="table w-max">
+                        <thead>
+                            <tr class="">
+                                <th></th>
+                                <th>In (Morning)</th>
+                                <th>Out (Morning)</th>
+                                <th>In (Afternoon)</th>
+                                <th>Out (Afternoon)</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr v-for="day in weekend">
+                                <th>{{ day.day }}</th>
+                                <td>
+                                    <InputForm type="time" :value='day.starts_at' />
+                                </td>
+                                <td>
+                                    <InputForm type="time" :value='day.ends_at' />
+                                </td>
+                                <td>
+                                    <InputForm type="time" :value='day.starts_at' />
+                                </td>
+                                <td>
+                                    <InputForm type="time" :value='day.ends_at' />
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <div class="flex justify-center mt-8">
+                    <button class="btn btn-outline btn-error mr-4">Cancel</button>
+                    <button class="btn btn-outline btn-success" @click="submit">Save</button>
                 </div>
             </div>
         </form>
