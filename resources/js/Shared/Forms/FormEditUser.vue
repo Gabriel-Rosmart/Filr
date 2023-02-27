40179563a18b46c7821833e5c85796da6c0e819f
Javier Mosquera <a21javiermr@iesteis.es>
2023-02-27 15:01:20 +0100

    fixed edit users

--- resources/js/Shared/Forms/FormEditUser.vue
+++ resources/js/Shared/Forms/FormEditUser.vue
@@ -14,7 +14,6 @@ const props = defineProps({
 })
 console.table(props.timetable);
 
-const weekend = [props.timetable[0], props.timetable[1], props.timetable[2], props.timetable[3], props.timetable[4]];
 const shift = ['Morning', 'Afternoon'];
 
 var form = useForm({
@@ -45,55 +44,7 @@ const submit = () => {
     <form @submit.prevent="" class="w-full pr-10">
         <div class="grid grid-cols-2 w-auto">
             <UserData :user="user" :form="form" :isAdmin="isAdmin" />
-            <!---
-                        <div class=" grid justify-items-center py-10 pl-10 ">
-                            <div class="grid grid-cols-2 w-max gap-6">
-                                <div class="grid row-span-3 justify-center items-center">
-                                    <div class="avatar">
-                                        <div class="mask mask-squircle w-60 h-60">
-                                            <img :src="storage + user.profile_pic" alt="Profile pic" />
-                                            </div>
-                                                    </div>
-                                                </div>
-                                                <InputForm title="Email" type="email" v-model="form.email" :value='user.email' />
-                                                <InputForm title="DNI" type="text" v-model="form.dni" :value='user.dni' />
-                                                <InputForm title="Phone" type="telephone" v-model="form.telephone" :value='user.phone' />
-                                                <SelectForm title="Role" :data='3' :rol="user.role_id" />
-                                                <InputForm title="Name" type="text" v-model="form.name" :value='user.name' />
-                                            </div>
-                                        </div>-->
-                <UserTimes :weekend="weekend" :shift="shift" />
-                <!--
-                                        <div class="flex justify-center">
-                                            <table class="table w-max">
-                                                <thead>
-                                                    <tr class="">
-                                                        <th></th>
-                                                        <th>In (Morning)</th>
-                                                        <th>Out (Morning)</th>
-                                                        <th>In (Afternoon)</th>
-                                                        <th>Out (Afternoon)</th>
-                                                    </tr>
-                                                </thead>
-                                                <tbody>
-                                                    <tr v-for="day in weekend">
-                                                        <th>{{ day.day }}</th>
-                                                        <td>
-                                                            <InputForm type="time" :value='day.starts_at' />
-                                                        </td>
-                                                        <td>
-                                                            <InputForm type="time" :value='day.ends_at' />
-                                                        </td>
-                                                        <td>
-                                                            <InputForm type="time" :value='day.starts_at' />
-                                                        </td>
-                                                        <td>
-                                                            <InputForm type="time" :value='day.ends_at' />
-                                                        </td>
-                                                    </tr>
-                                                </tbody>
-                                            </table>
-                                        </div>-->
+                <UserTimes :weekend="timetable" :shift="shift" />
                 <div class="flex justify-center mt-8">
                     <button class="btn btn-outline btn-error mr-4">{{ t('admin.buttons.cancel') }}</button>
                     <button class="btn btn-outline btn-success" @click="submit">{{ t('admin.buttons.save') }}</button>
