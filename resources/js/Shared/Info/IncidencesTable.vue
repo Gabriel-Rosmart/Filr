6a3ef2074e110d9b1fbb126174f0e79b2aed887c
Rodrigo Lopez <rodrilbsdn22@gmail.com>
2023-02-27 09:46:00 +0100

    Edit cando non se atopen incidencias nun día

--- resources/js/Shared/Info/IncidencesTable.vue
+++ resources/js/Shared/Info/IncidencesTable.vue
@@ -1,23 +1,23 @@
 <script setup>
 
-    /** Component imports */
-    import { Link } from '@inertiajs/inertia-vue3'
+/** Component imports */
+import { Link } from '@inertiajs/inertia-vue3'
 
-    /** Function imports */
-    import getIncidenceMessage from '@/Utilities/incidence';
-    import format from '@/Utilities/datefm'
+/** Function imports */
+import getIncidenceMessage from '@/Utilities/incidence';
+import format from '@/Utilities/datefm'
 
-    import { useI18n } from 'vue-i18n'
+import { useI18n } from 'vue-i18n'
 
-    const { t, d } = useI18n()
+const { t, d } = useI18n()
 
-    defineProps({
-        incidences: Array
-    })
+defineProps({
+    incidences: Array
+})
 </script>
 
 <template>
-    <div class="overflow-x-auto">
+    <div v-if="incidences.length > 0" class="overflow-x-auto">
         <table class="table w-full">
             <thead>
                 <tr>
@@ -28,11 +28,25 @@
             </thead>
             <tbody>
                 <tr v-for="incidence of incidences">
-                    <td><Link class="dark:hover:text-cyan-400 hover:underline" :href="'/admin/details?id=' + incidence.user.id">{{ incidence.user.name }}</Link></td>
+                    <td>
+                        <Link class="dark:hover:text-cyan-400 hover:underline"
+                            :href="'/admin/details?id=' + incidence.user.id">{{ incidence.user.name }}</Link>
+                    </td>
                     <td>{{ d(incidence.date, 'short') }}</td>
-                    <td v-html="t(`table.${incidence.subject}`, { minutes: incidence.minutes})"></td>
+                    <td v-html="t(`table.${incidence.subject}`, { minutes: incidence.minutes })"></td>
                 </tr>
             </tbody>
         </table>
     </div>
-</template>
+    <div v-else>
+        <div class="alert alert-info shadow-lg">
+            <div>
+                <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current flex-shrink-0 h-6 w-6" fill="none"
+                    viewBox="0 0 24 24">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                        d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                </svg>
+                <span>{{ t('admin.details.noCorrespondences') }}</span>
+            </div>
+        </div>
+</div></template>
