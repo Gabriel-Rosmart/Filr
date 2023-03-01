4d053b7eaac0df03ed725d7acf7c01f6982d03cc
Álvaro Sierra <asp.sierra@gmail.com>
2023-03-01 11:07:13 +0100

    Chaged layout in permit request

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -103,6 +103,6 @@ class UserController extends Controller
 
     public function permitRequest()
     {
-        return Inertia::render('User/PermitRequest');
+        return Inertia::render('User/PermitRequest', ['isAdmin' => Auth::user()->is_admin]);
     }
 }
--- resources/js/Pages/User/PermitRequest.vue
+++ resources/js/Pages/User/PermitRequest.vue
@@ -1,16 +1,27 @@
 <script setup>
-    import UserLayout from '@/Layouts/UserLayout.vue';
-    import Breadcrumbs from '@/Shared/Navigation/Breadcrumbs.vue';
-    import PermitForm from '@/Shared/User/PermitForm.vue';
+import UserLayout from '@/Layouts/UserLayout.vue';
+import AdminLayout from '@/Layouts/AdminLayout.vue'
+import Breadcrumbs from '@/Shared/Navigation/Breadcrumbs.vue';
+import PermitForm from '@/Shared/User/PermitForm.vue';
+import { Head } from '@inertiajs/inertia-vue3';
 
-    import { useI18n } from 'vue-i18n';
+import { useI18n } from 'vue-i18n';
 
-    const { t } = useI18n();
+const props = defineProps({
+    isAdmin: Number
+})
+
+const { t } = useI18n();
 </script>
 
 <template>
-    <UserLayout>
-        <Breadcrumbs class="ml-5 mt-6" :pages="[[t('breadcrumbs.user'), '/user'], [t('breadcrumbs.permitRequest'), '/user/permitreq']]" />
-        <PermitForm class="flex items-center m-8"/>
-    </UserLayout>
+    <Head title="Permit request" />
+
+    <component :is="isAdmin == 0 ? UserLayout : AdminLayout">
+        <Breadcrumbs v-if="isAdmin == 0" class="ml-5 mt-6"
+            :pages="[[t('breadcrumbs.user'), '/user'], [t('breadcrumbs.edit'), '/user/edit']]" />
+        <Breadcrumbs v-else-if="isAdmin == 1" class="ml-5 mt-6"
+            :pages="[['Admin', '/admin'], [t('breadcrumbs.dashboard'), '/admin']]" />
+        <PermitForm class="flex items-center m-8" />
+    </component>
 </template>
