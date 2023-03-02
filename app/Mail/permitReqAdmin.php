5df2efd2fa44248844b1b1671413807f5e73a218
Raul Arias Perez <aprariasperez@gmail.com>
2023-03-02 11:29:07 +0100

    attachments

--- app/Mail/permitReqAdmin.php
+++ app/Mail/permitReqAdmin.php
@@ -5,6 +5,7 @@ namespace App\Mail;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Attachment;
 use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
@@ -66,6 +67,10 @@ class permitReqAdmin extends Mailable
      */
     public function attachments()
     {
-        return [];
+        return [
+            Attachment::fromPath(storage_path('app/permitDocs/' . $this->uuid . '.pdf'))
+                ->as('justificante.pdf')
+                ->withMime('application/pdf'),
+        ];
     }
 }
