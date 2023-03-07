3021f2d6b668e680681f4077a8c69eb7467a96bb
Gabriel-Rosmart <66219601+Gabriel-Rosmart@users.noreply.github.com>
2023-03-07 08:37:19 +0100

    fix last commit

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,11 @@ import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
 // ! Uncoment when app ready to deploy, meanwhile work whit this commented, as make page reload faster
-
+/*
 import { resolve, dirname } from 'node:path'
 import { fileURLToPath } from 'url'
 import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite'
-
+*/
 export default defineConfig({
     plugins: [
         laravel({
@@ -22,9 +22,9 @@ export default defineConfig({
                 },
             },
         }),
-        
+        /*
         VueI18nPlugin({
             include: resolve(dirname(fileURLToPath(import.meta.url)), './resources/js/Locales/**'),
-        }),
+        }),*/
     ],
 });
