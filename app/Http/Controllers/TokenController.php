fe697ac6aab54eee2b3a6a099c96c199ee52e370
Álvaro Sierra <asp.sierra@gmail.com>
2023-05-19 19:39:15 +0200

    check posted data is ok, added some logs

--- app/Http/Controllers/TokenController.php
+++ app/Http/Controllers/TokenController.php
@@ -4,8 +4,10 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Carbon\Carbon;
+use Error;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 
 class TokenController extends Controller
 {
@@ -18,7 +20,19 @@ class TokenController extends Controller
     public function __invoke(Request $request)
     {
         //$user_id = request()->input('token');
+        //*Create a log to show submitted data     
+        Log::channel('daily')->info('INFO; Requested data = ' . $this->getSubmittedData($request->request));
+        try {
+            //Check if requested data are the expected
+            $this->checkData($request);
+            Log::channel('daily')->info('INFO; Data OK');
 
+        } catch (\Error $err) {
+            Log::channel('daily')->error(get_class($err) . "; " . $err->getMessage());
+            return $err->getMessage();
+        }
+
+/*
         // * Get date and hour
         $date = Carbon::parse($request->date)->format("Y-m-d");
         $time = Carbon::parse($request->time)->format("H:i:s");
@@ -35,6 +49,29 @@ class TokenController extends Controller
             'timestamp' => $time
         ]);*/
     
-        return "User " . $user->name . " clocked in succesfuly at $time on $date";
+        //return "User " . $user->name . " clocked in succesfuly at $time on $date";
+    }
+    /**
+     * Obtain a string formed by the requested date names and values
+     * @param   Object  $data   Data Received
+     * @return  String          Names and values
+     */
+    public function getSubmittedData($data){
+        $submittedData = "";
+        foreach ($data as $key => $value) {
+            $submittedData .= " $key($value) ";
+        }
+        return $submittedData;
+    }
+
+    /**
+     * Check if the requested data fulfills the expected
+     * parameters
+     * @param   Object  $data  Data received
+     */
+    public function checkData($data){
+        if(!isset($data->id) || !isset($data->date) || !isset($data->time)){
+            throw new Error("Submited data is not valid || Expected id(Number) date(Y-m-d) time(H:i:s) || Received " . $this->getSubmittedData($data->request));           
+        }
     }
 }
