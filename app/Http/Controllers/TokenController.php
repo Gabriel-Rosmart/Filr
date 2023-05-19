4fa8a0f7a3b2d4acd8b43fdd4c743e7683066f51
Álvaro Sierra <asp.sierra@gmail.com>
2023-05-19 19:06:09 +0200

    Retrieving user name and file date from request

--- app/Http/Controllers/TokenController.php
+++ app/Http/Controllers/TokenController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
@@ -16,23 +17,24 @@ class TokenController extends Controller
      */
     public function __invoke(Request $request)
     {
-
-        dd($request);
-        /*$user_id = request()->input('token');
+        //$user_id = request()->input('token');
 
         // * Get date and hour
-        $now = Carbon::now();
-        $now->setTimezone("GMT+1");
-        $date = $now->format("Y-m-d");
-        $time = $now->format("H:i:s");
+        $date = Carbon::parse($request->date)->format("Y-m-d");
+        $time = Carbon::parse($request->time)->format("H:i:s");
+
+        // * Get user name
+        $user = User::select('name')
+            ->where('id', $request->id)
+            ->first();
 
         // * Insert record on database
-        DB::table('files')->insert([
+        /*DB::table('files')->insert([
             'user_id' => $user_id,
             'date' => $date,
             'timestamp' => $time
-        ]);
-
-        return "Clock in succesfuly";*/
+        ]);*/
+    
+        return "User " . $user->name . " clocked in succesfuly at $time on $date";
     }
 }
