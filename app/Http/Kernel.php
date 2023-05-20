30f7e401e7340bbca79f00bf7f01803cf9863833
Gabriel Rosmart <gabrielrostron@gmail.com>
2023-05-20 19:08:21 +0200

    Fixed locale not changing

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -66,5 +66,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'admin' => \App\Http\Middleware\EnsureIsAdmin::class,
+        'locale' => \App\Http\Middleware\Locale::class
     ];
 }
