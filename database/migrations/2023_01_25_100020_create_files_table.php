e07f076c67c278ef7d8bf3c66c0b922069180a98
Raúl Arias Pérez <aprariasperez@gmail.com>
2023-05-22 13:51:22 +0200

    testing trigger library

--- database/migrations/2023_01_25_100020_create_files_table.php
+++ database/migrations/2023_01_25_100020_create_files_table.php
@@ -4,6 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\DB;
+use Triggers\Trigger;
 
 return new class extends Migration
 {
@@ -21,6 +22,20 @@ return new class extends Migration
             $table->time('timestamp')->nullable(false);
             //$table->string('day')->nullable(false)->default(DB::raw('DAYNAME(date)'));
         });
+
+        /* DB::unprepared('DROP TRIGGER IF EXISTS `files_incidences_insert`');
+        DB::unprepared('CREATE TRIGGER `files_incidences_insert` AFTER INSERT ON `files` FOR EACH ROW
+        BEGIN
+            INSERT INTO incidences (user_id, date, subject, minutes, created_at, updated_at)
+            VALUES (NEW.user_id, NEW.date, "late", 5, NOW(), NOW());
+        END'); */
+
+        Trigger::table('files')->afterInsert(function()
+        {
+            
+        });
+
+
     }
 
     /**
