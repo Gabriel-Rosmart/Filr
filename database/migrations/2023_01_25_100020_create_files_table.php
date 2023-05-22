2988738a0eb89e28eb159f3d20ca61e034c69f12
Raúl Arias Pérez <aprariasperez@gmail.com>
2023-05-22 14:13:17 +0200

    deleted library

--- database/migrations/2023_01_25_100020_create_files_table.php
+++ database/migrations/2023_01_25_100020_create_files_table.php
@@ -4,7 +4,6 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\DB;
-use Triggers\Trigger;
 
 return new class extends Migration
 {
@@ -23,17 +22,12 @@ return new class extends Migration
             //$table->string('day')->nullable(false)->default(DB::raw('DAYNAME(date)'));
         });
 
-        /* DB::unprepared('DROP TRIGGER IF EXISTS `files_incidences_insert`');
+        DB::unprepared('DROP TRIGGER IF EXISTS `files_incidences_insert`');
         DB::unprepared('CREATE TRIGGER `files_incidences_insert` AFTER INSERT ON `files` FOR EACH ROW
         BEGIN
             INSERT INTO incidences (user_id, date, subject, minutes, created_at, updated_at)
             VALUES (NEW.user_id, NEW.date, "late", 5, NOW(), NOW());
-        END'); */
-
-        Trigger::table('files')->afterInsert(function()
-        {
-            
-        });
+        END');
 
 
     }
