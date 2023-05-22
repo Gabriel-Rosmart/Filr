fffb0a51aea02f370e08b240329ffacfb8c4146f
Raúl Arias Pérez <aprariasperez@gmail.com>
2023-05-22 16:07:48 +0200

    gets schedule for curdate

--- app/Listeners/checkFileOnTime.php
+++ app/Listeners/checkFileOnTime.php
@@ -4,8 +4,10 @@ namespace App\Listeners;
 
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Support\Facades\DB;
 use App\Models\File;
 use App\Models\Incidence;
+use App\Models\User;
 
 class checkFileOnTime
 {
@@ -29,6 +31,17 @@ class checkFileOnTime
     {
         $file = $event->file;
 
-        dd($file);
+        $timetable = DB::table('schedules as s')
+            ->select('day', 'starts_at', 'ends_at')
+            ->join('date_ranges as ranges', 's.date_range_id', '=', 'ranges.id')
+            ->join('date_range_user as u_ranges', 'ranges.id', '=', 'u_ranges.id')
+            ->join('users as u', 'u_ranges.user_id', '=', 'u.id')
+            ->where('u.id', $file['user_id'])
+            ->where('ranges.start_date', '<=', DB::raw('curdate()'))
+            ->where('ranges.end_date', '>=', DB::raw('curdate()'))
+            ->where('s.day', '=', DB::raw('dayofweek(curdate())'))
+            ->get();
+
+        dd($timetable);
     }
 }
